73b5ba6a2f107c0df49a1b0225c13d1cc1b649ea
Tarazonaa <andres.tara.so@gmail.com>
2023-09-13 17:57:14 -0600

    Cambio a juego del cañon

--- cannon.py
+++ cannon.py
@@ -23,8 +23,8 @@ def tap(x, y):
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-        speed.x = (x + 200) / 25
-        speed.y = (y + 200) / 25
+        speed.x = (x + 250) / 25
+        speed.y = (y + 250) / 25
 
 
 def inside(xy):
