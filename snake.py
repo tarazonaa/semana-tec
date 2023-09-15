65996555bf64f2ebbaab397016f152c5f80aa490
tsommerp <a01781292@tec.mx>
2023-09-15 09:10:17 -0600

    La comida se mueve al azar un paso a la vez

--- snake.py
+++ snake.py
@@ -43,8 +43,8 @@ def move():
 
     if head == food:
         print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
+        food.x = randrange(-1, 1) * 10
+        food.y = randrange(-1, 1) * 10
     else:
         snake.pop(0)
 
